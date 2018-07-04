69a4aaf9dafaed2a2e206029125a9d3c502b01dd
陈雅凯 <chenyakai@zhuojianchina.com>
2018-07-04 09:10:29 +0800

    fans.js

--- fans.js
+++ fans.js
@@ -4,40 +4,43 @@ class Fan {
         this._level = ''
     }
     control(level) {
-        level === 0 ? console.log('Turn off the fan') :
-        console.log('Adjust the fans\'s speed to ' + level * 100 + 'r/min')
+        level === 0 ? console.log('off') :
+        console.log('level: ' + level)
     }
     increaseLevel() {
-        ++this._level > this._maxlevel ? console.log('Can\' be increased.') : this.control(this._level)
+        ++this._level > this._maxlevel ? console.log('max, can\'t increase') : this.control(this._level)
         this._level = this._level > this._maxlevel ? this._maxlevel : this._level
     }
     decreaseLevel() {
-        --this._level < 0 ? console.log('Can\' be decreased.') : this.control(this._level)
+        --this._level < 0 ? console.log('min, can\'t decrease') : this.control(this._level)
         this._level = this._level < 0 ? 0 :this._level
     }
 }
 class TeleControlFan extends Fan {
-    constructor() {
-        super()
+    constructor(maxlevel) {
+        super(maxlevel)
     }
-    jumptoLevel(level) {
+    jumptoLevel(level=1) {
+        level > -1 && level < this._maxlevel ? this.control(level) : console.log('beyond the fan\'s level')
         this._level = level
-        this.control(this._level)
     }
 }
-var thefan = new Fan(5)
+var thefan = new Fan()
 thefan.decreaseLevel()
 thefan.increaseLevel()
 thefan.decreaseLevel()
-thefan.decreaseLevel()
 thefan.increaseLevel()
 thefan.increaseLevel()
 thefan.increaseLevel()
 thefan.increaseLevel()
-var theTeleControlFan = new TeleControlFan()
-theTeleControlFan.jumptoLevel(2)
-theTeleControlFan.jumptoLevel(0)
+
+var theTeleControlFan = new TeleControlFan(100)
+theTeleControlFan.jumptoLevel(-1)
+theTeleControlFan.jumptoLevel(20)
+theTeleControlFan.decreaseLevel()
 theTeleControlFan.increaseLevel()
+theTeleControlFan.jumptoLevel()
+theTeleControlFan.jumptoLevel(0)
 
 
 
